24dffffd3d08d2694584da45b7041e4e3988e4b6
David Scott <dave9860@gmail.com>
2024-06-14 12:49:18 -0600

    Question 1, Part 1

---
+++ sql_queries/Q1_Part1.sql
@@ -0,0 +1,19 @@
+--The number of products in each price bracket.
+SELECT
+    pc.name AS category_name,
+    MIN(pch.standardcost) AS lowest_price,
+    MAX(pch.standardcost) AS highest_price,
+    MAX(pch.standardcost) - MIN(pch.standardcost) AS price_difference,
+    COUNT(p.productid) AS product_count
+FROM
+    production.productcategory pc
+JOIN
+    production.productsubcategory psc ON pc.productcategoryid = psc.productcategoryid
+JOIN
+    production.product p ON p.productsubcategoryid = psc.productsubcategoryid
+JOIN
+    production.productcosthistory pch ON p.productid = pch.productid
+GROUP BY
+    pc.name
+ORDER BY
+    pc.name;
